2c3599b09abb357af53f1faef503eb0769b610b5
Karina1014 <janneth_wi15@hotmail.com>
2024-11-12 15:38:13 -0500

    change program wv

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
-require 'sinatra'
+require 'webrick'
 
-set :port, ENV['PORT'] || 4567
+# Obtiene el puerto desde la variable de entorno o usa 4567 por defecto
+port = ENV['PORT'] || 4567
 
-get '/' do
-  "Hello, world!"
+# Crea el servidor HTTP
+server = WEBrick::HTTPServer.new :Port => port
+
+# Define una ruta para la raíz "/"
+server.mount_proc '/' do |req, res|
+  res.content_type = 'text/html'
+  res.body = "<h1>Hello from Ruby HTTP Server!</h1>"
+  res.body = "<h1>Developer name: Willian Vargas!</h1>"
 end
+
+# Inicia el servidor
+trap('INT') { server.shutdown }
+server.start
